96d459b8a9d413704819433a78add6d3b9a184d8
Rohit Upadhya <rohitupadhya18@gmail.com>
2021-05-24 12:36:28 +0530

    Navbar issue fixed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 const App = () => (
   <div className='background'>
-    <Navigation style={{top: '0', position: "fixed"}} />
+    <Navigation />
      <Home />
     <Footer />
   </div>
